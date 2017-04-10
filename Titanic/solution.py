c0847a0c7444bf54180c7dfa7be4bad625fe736c
guangleiw <stylite.wang@gmail.com>
2017-04-10 14:03:52 +0800

    Add solution and data for Titanic

---
+++ Titanic/solution.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import sys
+
+PATH=sys.path[0]
+#print(PATH)
+
+###
+# Acquire Data
+###
+# download data from here:
+# https://www.kaggle.com/c/titanic/data
+train_src=pd.read_csv(PATH+"/data/train.csv")
+test_src=pd.read_csv(PATH+"/data/test.csv")
+#print(test_src.head())
+
+###
+# Analyze by describing data
+# Q: You should identify which features are numerical(discrete / continous) , categorical and mixed data types 
+# A: categorical: survived sex embarked; ordinal: Pclass
+# A: numerical: continous (age fare) discrete(sibsp parch)
+# Q: Which features are mixed data types?
+# A: Ticket is a mix of numerical and alphanumeric data types; Cabin is alphanumeric
+### 
+# Q: Whice features may contain errors or typos?
+# A: This is harder to review for a large dataset, 
+# A: however reviewing a few samples from a smaller dataset may just tell us outright, 
+# Q: which features may require correcting.
+# A: Name feature may contain errors or typos as there are several ways used to describe a name including titles, 
+# A: round brackets, and quotes used for alternative or short names.
+# Q: Which features contain blank, null or empty values?
+# A: Cabin > Age > Embarked (training data) Cabin > Age are incomplete of test dataset
+# Q: What are the data types for various features?
+###
+#print(type(train_src))
+#print(train_src.axes)
+#print(train_src.tail())
+#print(train_src.info())
+#print('+'*40)
+#print(test_src.info())
+print(train_src.describe(include=['O']))
