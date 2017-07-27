4647b8fefaefa6ce464fb41aaa37eb6c05b373ea
guangleiw <stylite.wang@gmail.com>
2017-07-27 16:40:11 +0800

    append element

--- pmml/minidom.py
+++ pmml/minidom.py
@@ -1,27 +1,37 @@
 from xml.dom import minidom
 from pandas import DataFrame
 
+from pandas import DataFrame
+df=DataFrame([-1.959,5.238,5.172],index=['intercept','var_num1','var_num2'])
+
+#print(df.index)
+
+
 #pmml = minidom.parse('single_audit_logreg.pmml')
 pmml = minidom.parse('lr.pmml')
 
 root = pmml.documentElement
 
 model = root.getElementsByTagName('GeneralRegressionModel')[0]
-nameNode = model.getElementsByTagName("RegressionTable")[0]
-nodeList = nameNode.getElementsByTagName("NumericPredictor")
+nameNodeList = model.getElementsByTagName("RegressionTable")
+#nodeList = nameNode.getElementsByTagName("NumericPredictor")
 #print("Total Numbers:"+len(nodeList))
-for ele in nodeList:
-    print(ele.attributes['name'].value)
-    #print(ele.attributes['label'].value)
-    ele.setAttribute('name','var_num1')
-    print(ele.attributes['name'].value)
-    #print(ele.attributes['label'].value)
-    #print(pmml.toxml())
-    break
-    
+#for ele in nodeList:
+    #print(ele.attributes['name'].value)
+    ##print(ele.attributes['label'].value)
+    #ele.setAttribute('name','var_num1')
+    #print(ele.attributes['name'].value)
+    ##print(ele.attributes['label'].value)
+    ##print(pmml.toxml())
+    #break
 
-from pandas import DataFrame
-#d={'intercept':-1.959,'var_num1':5.238,'var_num2':5.172}
-#print(type(d))
-df=DataFrame([-1.959,5.238,5.172],index=['intercept','var_num1','var_num2'])
-print(df.head(10))
+for index,row in df.iterrows():
+    #print(type(index[0]))
+    #print(type(row[0]))
+    newEle = pmml.createElement("NumericPredictor")
+    newEle.setAttribute("name",index[0])
+    newEle.setAttribute("exponent","1")
+    newEle.setAttribute("coefficient",row[0])
+    nameNodeList[0].appendChild(newEle)
+
+print(pmml.toxml())
